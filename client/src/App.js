4ee7e67a0ba739fd1a442acc37dd1014bd27fca1
Dhawan Solanki <dhawansolanki2103@gmail.com>
2023-08-19 00:06:17 +0530

    Login UI Fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './login';
-
+import "./App.css"
 function App() {
-  <>
-
-    
-    <BrowserRouter>
-    <Routes>
-  
-    <Route path="/login" element={<LoginPage/> } />
-    {/* <Route path="*" element={<Error404/>} /> */}
-    </Routes>
-    </BrowserRouter>
-    
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+          {/* <Route path="*" element={<Error404 />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
-    
-
+  );
 }
 
 export default App;
--- client/src/login.js
+++ client/src/login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const LoginPage = () => {
+  console.log("Inside")
   return (
     <div className="relative min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 bg-gray-500 bg-no-repeat bg-cover relative items-center"
          style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1525302220185-c387a117886e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80)' }}>
