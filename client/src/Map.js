222883df915dfab3046cb5511e05ca4a639bff19
Dhawan Solanki <dhawansolanki2103@gmail.com>
2023-08-19 16:58:05 +0530

    Logs

--- client/src/Map.js
+++ client/src/Map.js
@@ -12,6 +12,7 @@ const MapPage = () => {
     axios.get(`https://api.seatgeek.com/2/events?client_id=MzU4MTY1MzV8MTY5MjQzNzM2OS4zODk0NDY3`)
       .then(response => {
         setEvents(response.data.events);
+        console.log(response.data.events); // Add this line to log the fetched data
       })
       .catch(error => {
         console.error('Error fetching data:', error);
@@ -26,11 +27,11 @@ const MapPage = () => {
     <div className="map">
       <MapContainer center={[initialLatitude, initialLongitude]} zoom={10} style={{ height: '500px' }}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        {events.map(event => (
+        {events.map(event =>  (
           <Marker key={event.id} position={[event.venue.location.lat, event.venue.location.lon]}>
             <Popup>
-              <div>
-                <h2>{event.title}</h2>
+              <div style={{"z-index":999}}>
+                <h2>{event.type}</h2>
                 <p>Date: {event.datetime_local}</p>
                 <a href={event.url} target="_blank" rel="noopener noreferrer">Event Details</a>
               </div>
